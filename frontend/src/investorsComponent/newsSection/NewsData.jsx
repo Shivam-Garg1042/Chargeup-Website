eefc3d27d87a657f25bae1e2e2afd555f851c5b4
unknown <20ucs183@lnmiit.ac.in>
2025-03-10 15:02:36 +0530

    first commit

--- frontend/src/investorsComponent/newsSection/NewsData.jsx
+++ frontend/src/investorsComponent/newsSection/NewsData.jsx
@@ -1,10 +1,15 @@
+import news1 from "../../assets/news1.jpg";
+import news2 from "../../assets/news2.jpg";
+import news3 from "../../assets/news3.jpg";
+import news4 from "../../assets/news4.jpg";
+
 export const newsData = [
   {
     id: 1,
     title: "Chargeup teams up for E3W financing",
     source: "Auto Economic Times",
     date: "June 5, 2023",
-    image: "/src/assets/news1.jpg",
+    image: {news1},
     color: "#000000",
     description: "Chargeup partners with Credit Fair and Ascend Capital to provide ₹100 crore in E3W financing for underserved drivers...",
       link : "https: auto.economictimes.indiatimes.com/news/commercial-vehicle/chargeup-collaborates-with-credit-fair-and-ascend-capital-for-e3w-financing-solutions/107880159"
@@ -14,7 +19,7 @@ export const newsData = [
       title: "Electric vehicles: Only commitment, and no real deal",
       source: "Auto Economic Times",
       date: "June 5, 2023",
-      image: "/src/assets/news2.jpg",
+      image: {news2},
       color: "#000000",
       description: "India's budget backs EVs but lacks clear plans or new subsidies...",
       link : "https: www.forbesindia.com/amp/article/news/electric-vehicles-only-commitment-and-no-real-deal/91199/1"
@@ -24,7 +29,7 @@ export const newsData = [
       title: "Chargeup to onboard 25,000 drivers in 17 cities by 2025",
       source: "Auto Economic Times",
       date: "June 5, 2023",
-      image: "/src/assets/news3.jpg",
+      image: {news3},
       color: "#000000",
       description: "Chargeup to onboard 25,000 drivers and expand battery stations across 17 cities...",
       link : "https: energy.economictimes.indiatimes.com/news/power/chargeup-to-onboard-25000-drivers-across-17-indian-cities-by-2025/109556468"
@@ -34,7 +39,7 @@ export const newsData = [
       title: "Chargeup finalist at Aegis Graham Bell Awards",
       source: "Auto Economic Times",
       date: "June 5, 2023",
-      image: "/src/assets/news4.jpg",
+      image: {news4},
       color: "#000000",
       description: "Chargeup honored as finalist in 14th Aegis Graham Bell Awards for innovation...",
       link : "https: www.linkedin.com/posts/aegis-graham-bell-award_congratulations-to-chargeup-finalist-under-activity-7298635481326465025-1xTl?utm_source=social_share_send&utm_medium=member_desktop_web&rcm=ACoAAA2bHUoBindJf1EnJhHgLL-nZm964iDkCvQ"
